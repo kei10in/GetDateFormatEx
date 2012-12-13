2ff4edfa84f7496057fc5e9a0fbb1fa6e3c513dc
kei10in <kei10in@gmail.com>
2012-12-13 20:57:38 +0900

    リファクタリング。

--- TestGetDateFormatEx/TestGetDateFormatEx.cpp
+++ TestGetDateFormatEx/TestGetDateFormatEx.cpp
@@ -54,33 +54,40 @@ INSTANTIATE_TEST_CASE_P(
 //////////////////////////////////////////////////////////////////////
 // Win32 API's GetDateFormatEx Test
 //////////////////////////////////////////////////////////////////////
+class TestWin32GetDateFormatEx_
+    : public ::testing::TestWithParam<DateFormatTestParam> {
+};
 
-TEST(TestWin32GetDateFormatEx, Format_yy_By2012Gets12) {
+TEST_P(TestWin32GetDateFormatEx_, FormatToString) {
+    DateFormatTestParam const& param(GetParam());
+    
     SYSTEMTIME st = {};
-    st.wYear = 2012;
-    st.wMonth = 1;
-    st.wDay = 2;
-    std::wstring actual(GetDateFormatEx(L"en-US", 0, &st, L"yy", nullptr));
-    std::wstring expected(L"12");
+    st.wYear = param.year;
+    st.wMonth = param.month;
+    st.wDay = param.day;
+    std::wstring actual(
+        GetDateFormatEx(L"en-US", 0, &st, param.format, nullptr));
+    std::wstring expected(param.expected);
     ASSERT_EQ(expected, actual);    
 }
 
-TEST(TestWin32GetDateFormatEx, FormatNullString) {
-    SYSTEMTIME st = {};
-    st.wYear = 2012;
-    st.wMonth = 1;
-    st.wDay = 2;
-    std::wstring actual(GetDateFormatEx(L"en-US", 0, &st, L"", nullptr));
-    std::wstring expected(L"");
-    ASSERT_EQ(expected, actual);    
+std::vector<DateFormatTestParam> GenWin32GetDateFormatTestParamList() {
+    static DateFormatTestParam params[] = {
+        { 2012, 8, 12, L"", L"" },
+        { 2012, 8, 12, L"yyyy", L"2012" },
+        { 2011, 8, 12, L"yyyy", L"2011" },
+        { 2012, 8, 12, L"yy", L"12" },
+        { 2009, 8, 12, L"yy", L"09" },
+        { 2009, 8, 12, L"y", L"9" },
+        { 2012, 8, 12, L"MMMM", L"August" },
+        { 2012, 8, 12, L"MMM", L"Aug" },
+        { 2012, 8, 12, L"MM", L"08" },
+        { 2012, 8, 12, L"M", L"8" },
+    };
+    return std::vector<DateFormatTestParam>(params, params + _countof(params));
 }
 
-TEST(TestWin32GetDateFormatEx, Format_MMM_By8GetsAug) {
-    SYSTEMTIME st = {};
-    st.wYear = 2012;
-    st.wMonth = 8;
-    st.wDay = 2;
-    std::wstring actual(GetDateFormatEx(L"en-US", 0, &st, L"MMM", nullptr));
-    std::wstring expected(L"Aug");
-    ASSERT_EQ(expected, actual);
-}
+INSTANTIATE_TEST_CASE_P(
+    DateFormat,
+    TestWin32GetDateFormatEx_,
+    ::testing::ValuesIn(GenWin32GetDateFormatTestParamList()));
